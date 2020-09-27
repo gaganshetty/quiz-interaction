fad1a3b163b93c853923278133bb85cf9deec50d
Gagan KG <gagan@piktorlabs.com>
2020-09-27 12:06:44 +0530

    Updates

---
+++ precache-manifest.eb0e8442d6ea5cf791e2eeb095134646.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "083903e9f996bf16df423083a1c5db7c",
+    "url": "/quiz-interaction/index.html"
+  },
+  {
+    "revision": "066b49d145627a1413f5",
+    "url": "/quiz-interaction/static/css/main.57750d61.chunk.css"
+  },
+  {
+    "revision": "e84308195abbab772f60",
+    "url": "/quiz-interaction/static/js/2.81746f37.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/quiz-interaction/static/js/2.81746f37.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "066b49d145627a1413f5",
+    "url": "/quiz-interaction/static/js/main.b8a14441.chunk.js"
+  },
+  {
+    "revision": "985e49eba73e112507b2",
+    "url": "/quiz-interaction/static/js/runtime-main.0228b2ae.js"
+  },
+  {
+    "revision": "658378531618bdd2fbe6ec29c360ea80",
+    "url": "/quiz-interaction/static/media/balloon-background-image.65837853.png"
+  },
+  {
+    "revision": "65bebea872c7cc5be00b792c2f650077",
+    "url": "/quiz-interaction/static/media/medal.65bebea8.svg"
+  }
+]);
