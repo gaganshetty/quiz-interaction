280616e311cf76e302a45c3e465532f76a355239
Gagan KG <gagan@piktorlabs.com>
2020-09-27 12:05:01 +0530

    Github pages configured

--- src/components/Result.js
+++ src/components/Result.js
@@ -1,5 +1,6 @@
 import React, {useEffect, useState} from 'react'
 import "../styles/Result.scss"
+import MedalImage from '../medal.svg'
 
 export default function Result({retry, score, total}) {
     const [percentage, setPercentage] = useState(0)
@@ -10,8 +11,9 @@ export default function Result({retry, score, total}) {
 
     return (
         <div className="result-container">
+            <img src={MedalImage} alt="Medal"/>
             <div className="aggregate-value">{percentage}</div>
-    <div className="message">{score !== 0 ? "Congratulations" : "Oops!"}</div>
+            <div className="message">{score !== 0 ? "Congratulations" : "Oops!"}</div>
             <div className="success-ratio">You got {score} out of {total} questions</div>
             <button className="Retry" onClick={retry}>Replay</button>
         </div>
